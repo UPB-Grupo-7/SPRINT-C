5e0140fdd5c17bc969b0293b40346c5ddac96682
Ela Pino <90581426+Elapino@users.noreply.github.com>
2021-09-27 20:30:55 -0500

    Add files via upload

---
+++ registro.js
@@ -0,0 +1,31 @@
+let registro = [];
+usuario = document.getElementById('in_usuario').value
+contrasena = document.getElementById('in_contrasena').value
+
+
+function agregarRegistro(){
+
+    //VALIDACIÓN
+   /* const ptr = new RegExp('^[A-Z0-9Ñ]+$', 'i');
+    if (usuario.length < 3 || usuario.length > 9  && contrasena.length < 6) {
+        console.log("longitud");
+    } else if (!ptr.test(string)) {
+        console.log("caracteres");
+    } else {
+        let persona = [usuario,contrasena]
+        registro.push(persona);
+        console.log(persona);
+    }*/
+    registro.push(persona);
+
+}
+
+agregarRegistro()
+
+function filtrarPorContrasena(arreglo, filtro){
+
+}
+
+module.export.registros = registros;
+module.export.filtrarPorContrasena = filtrarPorContrasena;
+module.export.agregarRegistro = agregarRegistro;
